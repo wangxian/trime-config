8328aea2fb419e56a012fd6b4455bd0149efaa02
wangxian <xian366@126.com>
2023-09-09 23:53:27 +0800

    修改：format rime.lua

--- rime.lua
+++ rime.lua
@@ -1,31 +1,52 @@
 function date_translator(input, seg)
-   if (input == "date") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
-   end
-   if (input == "time") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
-	  yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
-   end
+    -- 输入完整日期
+    if (input == "datetime") then
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M:%S"), ""))
+    end
+
+    -- 输入日期
+    if (input == "date") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
+    end
+
+    -- 输入时间
+    if (input == "time") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
+    end
+
+    -- 输入星期
+    -- -- @JiandanDream
+    -- -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
+    if (input == "week") then
+        local weekTab = {'日', '一', '二', '三', '四', '五', '六'}
+        yield(Candidate("week", seg.start, seg._end, "周"..weekTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, "星期"..weekTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, os.date("%A"), ""))
+        yield(Candidate("week", seg.start, seg._end, os.date("%a"), "缩写"))
+        yield(Candidate("week", seg.start, seg._end, os.date("%W"), "周数"))
+    end
 end
 
 --- 过滤器：单字在先
 function single_char_first_filter(input)
-   local l = {}
-   for cand in input:iter() do
-      if (utf8.len(cand.text) == 1) then
-         yield(cand)
-      else
-         table.insert(l, cand)
-      end
-   end
-   for i, cand in ipairs(l) do
-      yield(cand)
-   end
+    local l = {}
+    for cand in input:iter() do
+        if (utf8.len(cand.text) == 1) then
+            yield(cand)
+        else
+            table.insert(l, cand)
+        end
+    end
+
+    for i, cand in ipairs(l) do
+        yield(cand)
+    end
 end
